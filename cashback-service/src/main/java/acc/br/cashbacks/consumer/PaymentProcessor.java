a1a7f806d3bca8c64f5459894ceca6910f662d1b
netocodess <nettoguedes316@gmail.com>
2025-05-22 16:53:03 -0300

    feat: add object in metodo

--- cashback-service/src/main/java/acc/br/cashbacks/consumer/PaymentProcessor.java
+++ cashback-service/src/main/java/acc/br/cashbacks/consumer/PaymentProcessor.java
@@ -1,11 +1,15 @@
 package acc.br.cashbacks.consumer;
 
+import acc.br.cashbacks.OrderStatusUpdatedEvent;
 import acc.br.cashbacks.service.PaymentService;
 import acc.br.orders.domain.TypeOfStatus;
 import acc.br.orders.domain.model.Orders;
+import acc.br.orders.dtos.OrderCreatedEvent;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class PaymentProcessor {
@@ -22,21 +26,26 @@ public class PaymentProcessor {
      * Consumir mensagens da fila de pedidos e simular pagamento.
      */
     @RabbitListener(queues = "order.v1.order-status.updated")
-    public void processOrder(Orders orders) {
-        System.out.println("Recebendo pedido para processamento: " + orders);
+    public void processOrder(OrderCreatedEvent event) {
+        System.out.println("Recebendo pedido para processamento: " + event);
 
-        paymentService.simulatePayment(orders);
+        OrderStatusUpdatedEvent updatedEvent = new OrderStatusUpdatedEvent(
+                event.orderId(),
+                TypeOfStatus.DELIVERED,
+                LocalDateTime.now()
+        );
 
-        orders.setStatus(TypeOfStatus.DELIVERED);
+        paymentService.simulatePayment(event);
 
-        System.out.println("Pedido processado e atualizado: " + orders);
-        System.out.println(orders.getStatus() + "!");
+        System.out.println("Pedido processado e atualizado: " + TypeOfStatus.DELIVERED);
+        System.out.println(TypeOfStatus.DELIVERED + "!");
+
 
         // Enviar o pedido atualizado para a fila de status
         rabbitTemplate.convertAndSend(
-                "order.exchange",     // Exchange correta
-                "order.created",           // Routing key para status atualizado
-                orders
+                "order.exchange",
+                "order.created",
+                updatedEvent
         );
     }
 }
--- cashback-service/src/main/java/acc/br/cashbacks/service/PaymentService.java
+++ cashback-service/src/main/java/acc/br/cashbacks/service/PaymentService.java
@@ -1,18 +1,19 @@
 package acc.br.cashbacks.service;
 
 import acc.br.orders.domain.model.Orders;
+import acc.br.orders.dtos.OrderCreatedEvent;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
-    public void simulatePayment(Orders orders) {
+    public void simulatePayment(OrderCreatedEvent event) {
         try {
-            System.out.println("Processando pagamento para o pedido ID: " + orders.getId_Pedido());
+            System.out.println("Processando pagamento para o pedido ID: " + event.orderId());
             Thread.sleep(4000); // Simula um atraso no pagamento
-            System.out.println("Pagamento concluído para o pedido ID: " + orders.getId_Pedido());
+            System.out.println("Pagamento concluído para o pedido ID: " + event.orderId());
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            System.err.println("Erro ao simular pagamento para o pedido ID: " + orders.getId_Pedido());
+            System.err.println("Erro ao simular pagamento para o pedido ID: " + event.orderId());
         }
     }
 }
